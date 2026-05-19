76a4ed541b99eee181ade2429943692b43c080ec
Laksh_tech <lakshdeep@itechnolabs.tech>
2026-05-19 23:34:36 +0530

    Worked on delhivary and kits

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -21,7 +21,7 @@
         </a>
         @endcanAccess
 
-        @canAccess('trucks.view')
+        <!-- @canAccess('trucks.view')
         <a href="{{ route('admin.trucks.index') }}" @click="sidebarOpen = false"
            class="ui-nav-link flex items-center px-4 py-3 text-sm font-semibold {{ request()->routeIs('admin.trucks.*') ? 'is-active' : '' }}">
             <i class="fas fa-truck mr-3 w-5 text-center"></i>
@@ -75,7 +75,7 @@
             <i class="fas fa-toolbox mr-3 w-5 text-center"></i>
             <span>Kits</span>
         </a>
-        @endcanAccess
+        @endcanAccess -->
 
         @canAccess('users.view')
         <a href="{{ route('admin.users.index') }}" @click="sidebarOpen = false"
